9952d6bde9aeec98c7ca15980d71af62c8acd87e
Sneha <sneharoy90737@gmail.com>
2026-01-29 22:04:57 +0530

    error fixed

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -1,22 +1,90 @@
+# from textSummarizer.config.configuration import ConfigurationManager
+# from transformers import AutoTokenizer
+# from transformers import pipeline
+# from transformers import PegasusForConditionalGeneration
+#
+#
+# class PredictionPipeline:
+#     def __init__(self):
+#         self.config = ConfigurationManager().get_model_evaluation_config()
+#
+#     def predict(self, text):
+#         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+#         # gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
+#         model = PegasusForConditionalGeneration.from_pretrained(
+#             self.config.model_path
+#         )
+#
+#         # pipe = pipeline("summarization", model=self.config.model_path, tokenizer=tokenizer)
+#         # pipe = pipeline("text-generation", model=self.config.model_path, tokenizer=tokenizer)
+#         pipe = pipeline(
+#             "summarization",
+#             model=model,
+#             tokenizer=tokenizer
+#         )
+#
+#         print("Dialogue:")
+#         print(text)
+#
+#         # output = pipe(text, **gen_kwargs)[0]["summary_text"]
+#         output = pipe(
+#             text,
+#             max_new_tokens=80,
+#             num_beams=8,
+#             length_penalty=0.8,
+#             do_sample=False
+#         )[0]["summary_text"]
+#
+#         print("\nModel Summary:")
+#         print(output)
+#
+#         return output
+
+
+
+
+
 from textSummarizer.config.configuration import ConfigurationManager
-from transformers import AutoTokenizer
-from transformers import pipeline
+from transformers import AutoTokenizer, PegasusForConditionalGeneration
+import torch
 
 
 class PredictionPipeline:
     def __init__(self):
         self.config = ConfigurationManager().get_model_evaluation_config()
+        self.tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        self.model = PegasusForConditionalGeneration.from_pretrained(
+            self.config.model_path
+        )
 
-    def predict(self, text):
-        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
-
-        pipe = pipeline("summarization", model=self.config.model_path, tokenizer=tokenizer)
-
+    def predict(self, text: str):
         print("Dialogue:")
         print(text)
 
-        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        text = "summarize: " + text
+
+        inputs = self.tokenizer(
+            text,
+            truncation=True,
+            padding="longest",
+            return_tensors="pt"
+        )
+
+        summary_ids = self.model.generate(
+            inputs["input_ids"],
+            num_beams=6,
+            max_new_tokens=60,
+            min_length=25,
+            length_penalty=1.0,
+            no_repeat_ngram_size=3,
+            early_stopping=True
+        )
+
+        output = self.tokenizer.decode(
+            summary_ids[0],
+            skip_special_tokens=True
+        )
+
         print("\nModel Summary:")
         print(output)
 
